fe561cfabd679d19472b009a1bd538c16c6c329c
Tyler Weimin Ouyang <ouyang@cs.ucsb.edu>
2016-08-23 01:27:30 -0700

    Forms are now loaded from templates. Code is DRY.

--- we1s-web/app/create/views.py
+++ we1s-web/app/create/views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, redirect, request
 
-
 create = Blueprint('create', __name__,
                    template_folder='templates')
 
@@ -11,6 +10,41 @@ def create_publication():
         print request.get_json()
         redirect('index')
 
-    return render_template('create/form.html',
+    return render_template('create/main.html',
                            formname='Publications Manifest Form',
-                           formfile='publicationform.js')
+                           formfile='create/publication.html')
+
+
+@create.route('/create/corpus')
+def create_corpus():
+    return render_template('create/corpus.html')
+
+
+@create.route('/create/collection')
+def create_collection():
+    return render_template('create/corpus.html')
+
+
+@create.route('/create/rawdata')
+def create_rawdata():
+    return render_template('create/corpus.html')
+
+
+@create.route('/create/processeddata')
+def create_processeddata():
+    return render_template('create/corpus.html')
+
+
+@create.route('/create/metadata')
+def create_metadata():
+    return render_template('create/corpus.html')
+
+
+@create.route('/create/outputs')
+def create_outputs():
+    return render_template('create/corpus.html')
+
+
+@create.route('/create/related')
+def create_related():
+    return render_template('create/corpus.html')
