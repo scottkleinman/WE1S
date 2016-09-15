35bba77ca25502e31fdb56caefea55ff4add8d83
scottkleinman <scott.kleinman@csun.edu>
2016-09-15 16:22:07 -0700

    Enabled mimetype detection for raw data display

--- we1s-web/app/mod_display/view.py
+++ we1s-web/app/mod_display/view.py
@@ -1,14 +1,14 @@
-import io
+import io, mimetypes
 
 from flask import render_template
 from flask import send_file
 
 from app import app, db
 
-
 @app.route('/display/rawdata/<name>')
 def display_raw_data(name):
     d = db.Corpus.find_one({'name': name})
+    mimetype = mimetypes.guess_type(name)[0] # Gets the mimetype from the extension
     if d:
-        return send_file(io.BytesIO(d['content']), mimetype='application/pdf')
+        return send_file(io.BytesIO(d['content']), mimetype=mimetype)
     return render_template('404.html'), 404
