102b8958a06727e4df221fe654885cacd15636bd
scottkleinman <scott.kleinman@csun.edu>
2016-10-24 17:23:15 -0700

    Switched jsonify to json.dumps

--- we1s-web/app/mod_display/view.py
+++ we1s-web/app/mod_display/view.py
@@ -35,7 +35,7 @@ def table_data():
 		pub_row += ['<button type="button" title="Edit" class="btn btn-default" id="edit" data-id="{0}"><span class="glyphicon glyphicon-pencil"></span></button><button type="button" title="Delete" class="btn btn-default" id="delete" data-id="{0}"><span class="glyphicon glyphicon-trash"></span></button><button type="button" title="Export Manifest" class="btn btn-default" id="export" data-id="{0}"><span class="glyphicon glyphicon-download"></span></button>'.format(data_id)]
 		table_data += [pub_row]
 
-	return jsonify(table_data)
+	return json.dumps(table_data)
 
 @app.route('/display/rawdata/<name>')
 def display_raw_data(name):
